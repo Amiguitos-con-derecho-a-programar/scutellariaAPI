1dc0cdc9ac628b0b928fcb370b793eef8b434424
SofiaGonzaloC <sofiagonzaloc@outlook.com>
2023-04-06 22:25:21 -0600

    final

--- server/app.js
+++ server/app.js
@@ -1,10 +1,6 @@
 import express from "express";
 import cors from "cors";
 import postsRoutes from "./routes/posts.routes.js";
-import laborDocumentsRoutes from "./routes/laborDocuments.routes.js";
-import employeesRoutes from "./routes/employees.routes.js";
-import usersRoutes from "./routes/users.routes.js";
-import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 //midleware
@@ -12,9 +8,4 @@ app.use(express.json());
 app.use(cors());
 //routes 
 app.use(postsRoutes);
-app.use(laborDocumentsRoutes);
-app.use(employeesRoutes)
-app.use(usersRoutes)
-app.use(authRoutes)
-
 export default app
--- server/controllers/auth.controller.js
+++
@@ -1,6 +0,0 @@
-import Users from "../models/Users.js";
-
-export const login = async (req, res) => {
-    res.send('ok')
-}
-
